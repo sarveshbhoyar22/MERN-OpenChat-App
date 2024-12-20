47a5ccf80843435e2cabd0a3027a8a379dca8d62
Sarvesh Bhoyar <134962436+sarveshbhoyar22@users.noreply.github.com>
2024-12-20 08:52:01 +0530

    Update App.jsx

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,6 +1,6 @@
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
-import Signup from "./pages/signup/Signup.jsx";
+import Signup from "./pages/signup/SignUp.jsx";
 import { Routes,Route } from "react-router-dom";
 import {Toaster} from 'react-hot-toast'
 import { useAuthContext } from "./context/AuthContext";
